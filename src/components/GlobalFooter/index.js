c2323714cecaaa9ff5f15becaf674f90d31ddc28
Olivia <olier_fm@163.com>
2020-08-06 23:51:53 +0800

    feat(客户案例和底部)

--- src/components/GlobalFooter/index.js
+++ src/components/GlobalFooter/index.js
@@ -1,15 +1,44 @@
 import { Link } from 'umi';
 import styles from './index.less';
 
+import logoIcm from '../../assets/customer/logo-icm.png';
+import logoCci from '../../assets/customer/logo-cci.png';
+import logoCcm from '../../assets/customer/logo-ccm.png';
+
 export default function () {
   return (
     <footer className={styles.footer}>
-      <div className={styles.units}>
-        <div className={styles.unit}></div>
-        <div className={styles.unit}></div>
-        <div className={styles.unit}></div>
+      <section className={`${styles['section']}`}>
+        <div className={styles['section-inner']}>
+          <div className={styles['customer']}>
+            <ul className={styles['customer-list']}>
+              <li className={styles['customer-item']}>
+                <a className={styles['customer-link']} href="http://www.icm.gov.mo/">
+                  <img src={logoIcm} alt="" />
+                </a>
+              </li>
+
+              <li className={styles['customer-item']}>
+                <a className={styles['customer-link']} href="http://www.macaucci.com/">
+                  <img src={logoCci} alt="" />
+                </a>
+              </li>
+
+              <li className={styles['customer-item']}>
+                <a className={styles['customer-link']} href="https://www.ccm.gov.mo/">
+                  <img src={logoCcm} alt="" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+
+      <div className={styles.right}>
+        <div className={styles['right-inner']}>
+          戀愛・電影館 Cinematheque・Passion © 2017
+        </div>
       </div>
-      <div className={styles.right}>戀愛・電影館 Cinematheque・Passion © 2017</div>
     </footer>
   );
 }
